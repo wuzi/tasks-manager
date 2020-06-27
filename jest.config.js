3745bc9d1ad9a7042288caba8f37a87f5b7c353f
Lucas Godoy <lcsdegodoy@outlook.com>
2020-06-27 16:17:40 -0300

    chore(jest): add collect coverage from setting

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
+  collectCoverageFrom: [
+    'src/controllers/**/*.{ts,tsx}',
+    'src/services/**/*.{ts,tsx}',
+  ],
 };
