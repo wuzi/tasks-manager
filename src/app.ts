6c33910262499edb4e5bca224f09f9b87717540b
Lucas Godoy <lcsdegodoy@outlook.com>
2020-06-27 20:49:44 -0300

    feat: add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
 import routes from './routes';
@@ -9,6 +10,7 @@ import swaggerOptions from './config/swagger';
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/v1', routes);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
