758b3c50721d92af35b3f96d50beed86e950e38b
Lucas Godoy <lcsdegodoy@outlook.com>
2020-06-27 09:54:08 -0300

    feat: add body-parser middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import express from 'express';
+import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
 import routes from './routes';
 import swaggerOptions from './config/swagger';
@@ -8,6 +9,7 @@ import swaggerOptions from './config/swagger';
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
 app.use('/v1', routes);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 
