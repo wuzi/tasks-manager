9a76923b188dbc4bebe99aa669b5eb3d87fdb72d
Lucas Godoy <lcsdegodoy@outlook.com>
2020-06-27 09:10:38 -0300

    feat: add logger util

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { createConnection } from 'typeorm';
 import app from './app';
 import dbConfig from './config/database';
+import logger from './utils/logger';
 
 createConnection(dbConfig).then(() => {
   app.listen(app.get('port'), () => {
-    console.log(`App is running at http://localhost:${app.get('port')}`);
+    logger('database', `App is running at http://localhost:${app.get('port')}`, 'info');
   });
 }).catch((err) => {
-  console.log(`${err}`);
+  logger('database', 'It was not possible to connect to the database', 'error');
+  logger('database', err.message, 'error');
 });
