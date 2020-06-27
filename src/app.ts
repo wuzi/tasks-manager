667f9082cbc0d3821c9303ce089ef7cf31f6586e
Lucas Godoy <lcsdegodoy@outlook.com>
2020-06-27 01:36:52 -0300

    feat: add endpoint to view swagger docs

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import routes from './routes';
+import swaggerOptions from './config/swagger';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use('/v1', routes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 
 export default app;
