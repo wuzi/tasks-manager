566946b9aa9148d3c2cd3498048decde7ce9a0f0
Lucas Godoy <lcsdegodoy@outlook.com>
2020-06-27 00:20:20 -0300

    feat: add findAll method for TaskService

---
+++ src/services/task.ts
@@ -0,0 +1,12 @@
+import { getConnection, FindManyOptions } from 'typeorm';
+import Task from '../models/task';
+
+export default class TaskService {
+  /**
+   * Find all tasks in the database and paginate the results
+   * @param options Defines a special criteria to find specific entities.
+   */
+  static findAll(options?: FindManyOptions<Task>): Promise<[Task[], number]> {
+    return getConnection().getRepository(Task).findAndCount(options);
+  }
+}
