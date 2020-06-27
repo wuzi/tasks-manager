086436a84039d9b9c7762b30585a9089ea047d24
Lucas Godoy <lcsdegodoy@outlook.com>
2020-06-27 00:41:57 -0300

    feat: add controller to find tasks with filters

---
+++ src/controllers/task.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { Like } from 'typeorm';
+import TaskService from '../services/task';
+
+export default class TaskController {
+  static async findAll(req: Request, res: Response) {
+    const take = req.query.limit ? parseInt(req.query.limit as string, 10) : 20;
+    const skip = req.query.page ? take * (parseInt(req.query.page as string, 10) - 1) : 0;
+    const order = { id: req.query.orderById as 'ASC' | 'DESC' };
+
+    const where: any = {};
+    if (req.query.title) where.title = Like(`%${req.query.title}%`);
+    if (req.query.description) where.description = Like(`%${req.query.description}%`);
+
+    const [tasks, total] = await TaskService.findAll({
+      take, skip, where, order,
+    });
+
+    return res.json({ tasks, total });
+  }
+}
