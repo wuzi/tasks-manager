72dda23eb7ef6ec777c0a55f9ed0de10e9ffe78c
Lucas Godoy <lcsdegodoy@outlook.com>
2020-06-27 22:58:12 -0300

    test: add health check test cases

---
+++ src/controllers/health.controller.test.ts
@@ -0,0 +1,46 @@
+import * as typeorm from 'typeorm';
+import HealthController from './health.controller';
+
+jest.mock('express');
+jest.mock('typeorm');
+
+const res = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn().mockReturnThis(),
+};
+
+beforeEach(() => {
+  res.json.mockClear();
+  res.status.mockClear();
+  (typeorm as jest.Mocked<any>).getConnection.mockClear();
+});
+
+describe('HealthController', () => {
+  describe('check', () => {
+    it('should return 200 if it has a database connection', async () => {
+      (typeorm as jest.Mocked<any>).getConnection.mockReturnValue({
+        manager: {
+          connection: {
+            isConnected: true,
+          },
+        },
+      });
+      HealthController.check({} as any, res as any);
+      expect(res.status).toBeCalledWith(200);
+      expect(res.json).toBeCalledWith({ message: expect.anything() });
+    });
+
+    it('should return 503 if database is not connected', async () => {
+      (typeorm as jest.Mocked<any>).getConnection.mockReturnValue({
+        manager: {
+          connection: {
+            isConnected: false,
+          },
+        },
+      });
+      HealthController.check({} as any, res as any);
+      expect(res.status).toBeCalledWith(503);
+      expect(res.json).toBeCalledWith({ message: expect.anything() });
+    });
+  });
+});
