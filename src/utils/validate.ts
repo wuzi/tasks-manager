768cc3655b9eaddb8914219a111df48f9da1721a
Lucas Godoy <lcsdegodoy@outlook.com>
2020-06-30 09:36:12 -0300

    docs(utils): add jsdoc to validate func

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -2,6 +2,11 @@ import { plainToClass } from 'class-transformer';
 import { ClassType } from 'class-transformer/ClassTransformer';
 import { validate as cvValidator } from 'class-validator';
 
+/**
+ * Validates an object using a class.
+ * @param cls - The class to be used as model
+ * @param plain - The object to be validated
+ */
 export default async function validate<T, V>(cls: ClassType<T>, plain: V) {
   const object = plainToClass(cls, plain);
   return cvValidator(object);
